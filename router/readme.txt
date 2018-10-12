66b539cbc6a4054dfecf611508c17a6213a1e444
Fobenga <pedro.h.s.s09@hotmail.com>
2018-10-12 15:32:36 -0300

    included main files
    included cpp file and readme.txt

---
+++ router/readme.txt
@@ -0,0 +1,8 @@
+What the program do:
+
+1 - Gets command 'ipconfig' information and puts it in a "ip" file;
+2 - Searchs for the first that is written the default gateway;
+3 - Delete every character of the line but the raw default gateway adress;
+4 - Creates a string from the adress;
+5 - Opens the string in the default user browser;
+6 - Deletes the "ip.txt" file, as it is only necessary for searching.
---
+++ router/router.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <windows.h>
+using namespace std;
+
+void connect( string word )
+{    
+    string base_URL = "http://";
+    string search_URL = base_URL + word;
+    ShellExecuteA(NULL, "open", search_URL.c_str(), NULL, NULL, SW_SHOWNORMAL);
+}
+
+int main()
+{
+	string line;
+	ifstream IPFile;
+	int offset; 
+	string search0 = "Default Gateway . . . . . . . . . : ";
+	system("ipconfig > ip");
+
+	// Create ipconfig lines to read from it.
+	IPFile.open ("ip"); 
+	if(IPFile.is_open())
+ 	{
+    	while(!IPFile.eof())
+    	{
+ 			getline(IPFile,line);
+ 		
+ 			// Gets the pointer to the position that ends the serach0 array
+ 			if ((offset = line.find(search0, 0)) != string::npos)
+ 			{
+ 				// Delete all the characters of the line but the adress
+ 				line.erase(0,39);
+ 				cout << "Opening gateway [ "<<  line <<" ]." <<  endl;
+				connect(line);
+ 				IPFile.close();
+ 				
+ 				// Delete file, only used for reading.
+ 				remove("ip");
+ 			}
+ 		}
+	}
+	return 0;
+}
+
